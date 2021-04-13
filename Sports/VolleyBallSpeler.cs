9461bc0f7ca32dcee127b37d6c4ce7e98c177e12
Matthijs Debacker <matthijsdebacker@hotmail.com>
2021-04-13 09:33:33 +0200

    Second

--- Sports/VolleyBallSpeler.cs
+++ Sports/VolleyBallSpeler.cs
@@ -100,7 +100,7 @@ namespace Sports
             _hoogte = hoogte;
             _nummer = nummer;
             _matches = matches;
-            _hoogsteScore = hoogste;
+            HoogsteScore = hoogste;
         }
 
     }
