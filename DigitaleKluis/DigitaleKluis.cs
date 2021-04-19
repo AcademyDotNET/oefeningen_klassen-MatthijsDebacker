08fde64500a280894325990cc726c1001cf2ffdb
Matthijs Debacker <matthijsdebacker@hotmail.com>
2021-04-19 11:45:48 +0200

    Oefening Digitale Kluis afgewerkt.

---
+++ DigitaleKluis/DigitaleKluis.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitaleKluis
+{
+    class DigitaleKluis
+    {
+        public DigitaleKluis(int code)
+        {
+            _code = code;
+        }
+
+        private int _aantalPogingen = 0;
+        private int _code;
+        private bool _canShowCode = true;
+
+        public bool CanShowCode
+        {
+            get { return _canShowCode; }
+            set { _canShowCode = value; }
+        }
+
+        public int Code 
+        { 
+            get 
+            {
+                if (CanShowCode)
+                {
+                    return _code;
+                }
+                else
+                {
+                    return -666;
+                }
+            } 
+            private set 
+            { 
+                _code = value; 
+            } 
+        }
+
+        public int CodeLevel 
+        {
+            get
+            {
+                return _code / 1000;
+            }
+        }
+
+        public bool TryCode(int code)
+        {
+            if(code == -666)
+            {
+                Console.WriteLine("Cheater.");
+                return false;
+            }
+            else if(code == _code)
+            {
+                Console.WriteLine("Deze code is geldig.");
+                return true;
+            }
+            else if(code != _code)
+            {
+                _aantalPogingen++;
+                Console.WriteLine($"Deze code is niet geldig. (Aantal pogingen: {_aantalPogingen})");
+                return false;
+            }
+            return false;
+        }
+    }
+}
---
+++ DigitaleKluis/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace DigitaleKluis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitaleKluis kluis1 = new DigitaleKluis(515);
+            DigitaleKluis kluis2 = new DigitaleKluis(16834);
+            DigitaleKluis kluis3 = new DigitaleKluis(13673);
+            DigitaleKluis kluis4 = new DigitaleKluis(211683);
+
+            kluis1.CanShowCode = false;
+            kluis4.CanShowCode = false;
+
+            Console.WriteLine();
+            Console.WriteLine($"Kluis 1 code level: {kluis1.CodeLevel}.");
+            Console.WriteLine($"Kluis 2 code level: {kluis2.CodeLevel}.");
+            Console.WriteLine($"Kluis 3 code level: {kluis3.CodeLevel}.");
+            Console.WriteLine($"Kluis 4 code level: {kluis4.CodeLevel}.");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Kluis 1 code: {kluis1.Code}.");
+            Console.WriteLine($"Kluis 2 code: {kluis2.Code}.");
+            Console.WriteLine($"Kluis 3 code: {kluis3.Code}.");
+            Console.WriteLine($"Kluis 4 code: {kluis4.Code}.");
+            Console.WriteLine();
+
+            int kluis = 0;
+            DigitaleKluis current = null;
+            do
+            {
+                kluis = AskForInt("Voor welke kluis wilt u de code raden?");
+                switch (kluis)
+                {
+                    case 1:
+                        current = kluis1;
+                        break;
+                    case 2:
+                        current = kluis2;
+                        break;
+                    case 3:
+                        current = kluis3;
+                        break;
+                    case 4:
+                        current = kluis4;
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (current == null);
+
+            int input = 0;
+            do
+            {
+                input = AskForInt($"Raad de code voor kluis {kluis}: ");
+                current.TryCode(input);
+            }while (input != current.Code) ;
+        }
+
+        static int AskForInt(string question, string errorResponse = "Invalid number, please try again", bool clear = false)
+        {
+            Console.WriteLine(question);
+            int input = 0;
+
+            string sInput = Console.ReadLine();
+            while (!int.TryParse(sInput, out input))
+            {
+                if (clear)
+                {
+                    Console.Clear();
+                }
+                Console.WriteLine(errorResponse);
+                sInput = Console.ReadLine();
+            }
+
+            return input;
+        }
+    }
+}
